64e0e71a76f7cbb35b6d3ba1f3cb324d8aba4b0a
rambhask <ram.bhaskar@stridehealth.com>
2016-12-04 10:25:08 -0500

    Initial Commit

---
+++ ios-stethescope/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ios-stethescope
+//
+//  Created by Ram Bhaskar on 12/4/16.
+//  Copyright © 2016 Ram Bhaskar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
